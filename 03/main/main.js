6a0fc634f9b3ef650eb25e0afba7bf67030f0621
Matheus Gabriel <mtheu.gbiel.odr@gmail.com>
2022-01-18 13:24:07 -0300

    03 - Spred & Rest

--- 03/main/main.js
+++ 03/main/main.js
@@ -1,3 +1,28 @@
+//-----SPREAD
+console.log("====SPREAD====");
+function cadastroPessoa(info)
+{
+    let novosDados = {
+        ...info,
+        cargo: "Programador",
+        codigo: "54AD32"
+    }
+    return novosDados;
+}
+
+console.log(cadastroPessoa({nome: "Fernando", sobrenome: "Garcia", anoContrato: 2019}))
+
+//----REST  
+console.log("====REST====");
+function listaNumeros(...numeros)
+{
+    console.log(numeros);
+}
+
+listaNumeros(1,3,2,4,8,9);
+
+//-----OPERAÇÕES DENTRO DE UM ARRAY
+console.log("\n====OPERAÇÕES DENTRO DE UM ARRAY====");
 function Cadastrar(usuarios, ...novosUsuarios) //rest
 {
     Usuarios = [...usuarios, ...novosUsuarios]; //spread
